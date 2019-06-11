e50dd6681c47c7731fff7548b6aefa014d929377
Zach Lasky <zacharylasky123@gmail.com>
2019-06-11 18:53:25 -0400

    finished methods officially

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -87,10 +87,19 @@ ticketPriceTotal = runners.reduce((acc, item) => {
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  
+// Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+let donationArray = [];
+runners.forEach(function(runner) {
+    if (runner.donation > 100) {
+        donationArray.push(runner.donation);
+    }
+})
 
 // Problem 2
+let smallDonations = runners.filter(donate => donate.donation < 50);
 
 // Problem 3
+let longLastName = runners.filter(name => name.last_name.length > 7);
